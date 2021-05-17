b151c4eb854d1bfcd54644b38c60872cfdf80e70
Anna Ciszak <aniac600@gmail.com>
2021-05-17 23:38:56 +0200

    Dodanie typów i nazw pól

--- client/public/BoardStuff.js
+++ client/public/BoardStuff.js
@@ -1,52 +1,52 @@
 /* eslint-disable camelcase */
-import * as PIXI from "pixi.js";
-import { Button } from "./Button";
-import { BoardPositions } from "./config";
+import * as PIXI from 'pixi.js'
+import { Button } from './Button'
+import { BoardPositions } from './config'
 
 const Piece = (x, y, color) => {
-    const circle = new PIXI.Graphics();
-    circle.beginFill(color);
-    circle.drawCircle(x, y, 11);
-    circle.endFill();
-    return circle;
-};
+  const circle = new PIXI.Graphics()
+  circle.beginFill(color)
+  circle.drawCircle(x, y, 11)
+  circle.endFill()
+  return circle
+}
 
 export const BoardStuff = (board, app, socket, player_name) => {
-    const cont = new PIXI.Container();
+  const cont = new PIXI.Container()
 
-    // PIECES
+  // PIECES
 
-    if (board.state.players[0] != undefined) {
-        const p1 = Piece(
-            BoardPositions[board.state.players[0].pos].x + 432,
-            BoardPositions[board.state.players[0].pos].y + 20,
-            0xff0000
-        );
-        cont.addChild(p1);
-    }
-    if (board.state.players[1] != undefined) {
-        const p2 = Piece(
-            BoardPositions[board.state.players[1].pos].x + 472,
-            BoardPositions[board.state.players[1].pos].y + 20,
-            0xff00ff
-        );
-        cont.addChild(p2);
-    }
-    if (board.state.players[2] != undefined) {
-        const p3 = Piece(
-            BoardPositions[board.state.players[2].pos].x + 432,
-            BoardPositions[board.state.players[2].pos].y - 20,
-            0x00ff00
-        );
-        cont.addChild(p3);
-    }
-    if (board.state.players[3] != undefined) {
-        const p4 = Piece(
-            BoardPositions[board.state.players[3].pos].x + 472,
-            BoardPositions[board.state.players[3].pos].y - 20,
-            0x1111ff
-        );
-        cont.addChild(p4);
-    }
-    return cont;
-};
+  if (board.state.players[0] != undefined) {
+    const p1 = Piece(
+      BoardPositions[board.state.players[0].pos].x + 432,
+      BoardPositions[board.state.players[0].pos].y + 20,
+      0xff0000
+    )
+    cont.addChild(p1)
+  }
+  if (board.state.players[1] != undefined) {
+    const p2 = Piece(
+      BoardPositions[board.state.players[1].pos].x + 472,
+      BoardPositions[board.state.players[1].pos].y + 20,
+      0xff00ff
+    )
+    cont.addChild(p2)
+  }
+  if (board.state.players[2] != undefined) {
+    const p3 = Piece(
+      BoardPositions[board.state.players[2].pos].x + 432,
+      BoardPositions[board.state.players[2].pos].y - 20,
+      0x00ff00
+    )
+    cont.addChild(p3)
+  }
+  if (board.state.players[3] != undefined) {
+    const p4 = Piece(
+      BoardPositions[board.state.players[3].pos].x + 472,
+      BoardPositions[board.state.players[3].pos].y - 20,
+      0x1111ff
+    )
+    cont.addChild(p4)
+  }
+  return cont
+}
--- client/public/UI.js
+++ client/public/UI.js
@@ -1,87 +1,88 @@
 /* eslint-disable camelcase */
-import * as PIXI from "pixi.js";
-import { Button } from "./Button";
-import { ButtonInactive } from "./ButtonInactive";
+import * as PIXI from 'pixi.js'
+import { Button } from './Button'
+import { ButtonInactive } from './ButtonInactive'
+import { BoardPositions } from './config'
 
-const UserFrame = (player_name = "-", c = "0", x, y) => {
-    const frame = new PIXI.Graphics();
-    frame.x = x;
-    frame.y = y;
-    frame.beginFill(0xb5bdb5);
-    frame.drawRect(0, 0, 211, 80);
-    frame.endFill();
-    const name = new PIXI.Text(player_name, {
-        align: "center",
-        fontSize: 24,
-        fontWeight: "600",
-    });
-    name.resolution = 3;
-    name.x = frame.width / 2 - name.width / 2;
-    const cash = new PIXI.Text(c, {
-        align: "center",
-        fontSize: 24,
-        fontWeight: "200",
-    });
-    cash.resolution = 3;
-    cash.x = frame.width / 2 - cash.width / 2;
-    cash.y = 40;
+const UserFrame = (player_name = '-', c = '0', x, y) => {
+  const frame = new PIXI.Graphics()
+  frame.x = x
+  frame.y = y
+  frame.beginFill(0xb5bdb5)
+  frame.drawRect(0, 0, 211, 80)
+  frame.endFill()
+  const name = new PIXI.Text(player_name, {
+    align: 'center',
+    fontSize: 24,
+    fontWeight: '600'
+  })
+  name.resolution = 3
+  name.x = frame.width / 2 - name.width / 2
+  const cash = new PIXI.Text(c, {
+    align: 'center',
+    fontSize: 24,
+    fontWeight: '200'
+  })
+  cash.resolution = 3
+  cash.x = frame.width / 2 - cash.width / 2
+  cash.y = 40
 
-    frame.addChild(name, cash);
+  frame.addChild(name, cash)
 
-    return frame;
-};
+  return frame
+}
 
-export const UI = (board, app, socket, player_name) => {
-    const cont = new PIXI.Container();
+export const UI = (board, app, socket, player_name, field) => {
+  const cont = new PIXI.Container()
 
-    // QUIT BUTTON
-    const quit = Button(10, 400, "QUIT", () => {
-        socket.emit("exit_room", board.state.room, player_name);
-        app.state.inGame = false;
-    });
+  // QUIT BUTTON
+  const quit = Button(10, 400, 'QUIT', () => {
+    socket.emit('exit_room', board.state.room, player_name)
+    app.state.inGame = false
+  })
 
-    // USER FRAMES
-    for (let i = 0; i < 4; i++) {
-        if (board.state.players[i] != undefined) {
-            const uf = UserFrame(
-                board.state.players[i].name,
-                board.state.players[i].cash,
-                i == 0 || i == 2 ? 10 : 231,
-                i == 0 || i == 1 ? 40 : 130
-            );
-            cont.addChild(uf);
-        } else {
-            const uf = UserFrame(
-                "-",
-                "0",
-                i == 0 || i == 2 ? 10 : 231,
-                i == 0 || i == 1 ? 40 : 130
-            );
-            cont.addChild(uf);
-        }
-    }
-
-    if (board.state.my_turn) {
-        // ROLL DICE
-        const roll = Button(10, 300, "ROLL", () => {
-            socket.emit("roll_dice", board.state.room, player_name);
-        });
-
-        // END TURN
-        const end_turn = Button(240, 300, "END TURN", () => {
-            socket.emit("end_turn", board.state.room, player_name);
-        });
-        cont.addChild(roll, end_turn);
+  // USER FRAMES
+  for (let i = 0; i < 4; i++) {
+    if (board.state.players[i] != undefined) {
+      const uf = UserFrame(
+        board.state.players[i].name,
+        board.state.players[i].cash,
+        i == 0 || i == 2 ? 10 : 231,
+        i == 0 || i == 1 ? 40 : 130
+      )
+      cont.addChild(uf)
     } else {
-        // ROLL DICE
-        const roll = ButtonInactive(10, 300, "ROLL");
-
-        // END TURN
-        const end_turn = ButtonInactive(240, 300, "END TURN");
-        cont.addChild(roll, end_turn);
+      const uf = UserFrame(
+        '-',
+        '0',
+        i == 0 || i == 2 ? 10 : 231,
+        i == 0 || i == 1 ? 40 : 130
+      )
+      cont.addChild(uf)
     }
+  }
 
-    cont.addChild(quit);
+  if (board.state.my_turn) {
+    // ROLL DICE
+    const roll = Button(10, 300, 'ROLL', () => {
+      socket.emit('roll_dice', board.state.room, player_name)
+    })
 
-    return cont;
-};
+    // END TURN
+    const end_turn = Button(240, 300, 'END TURN', () => {
+      socket.emit('end_turn', board.state.room, player_name)
+    })
+    cont.addChild(roll, end_turn)
+  } else {
+    // ROLL DICE
+    const roll = ButtonInactive(10, 300, 'ROLL')
+
+    // END TURN
+    const end_turn = ButtonInactive(240, 300, 'END TURN')
+    cont.addChild(roll, end_turn)
+  }
+
+  cont.addChild(quit)
+
+  return cont
+}
